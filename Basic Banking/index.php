e0a1c967da52d831f3a9c2a221cdbfe0ea068d25
Sammyabhi <87541023+Sammyabhi@users.noreply.github.com>
2021-07-16 22:45:50 +0530

    Add files via upload

---
+++ Basic Banking/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+    <title>Triples Bank Of India</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong">
+    <style>
+        body {
+        padding-top: 60px;
+        font-size:25px;
+        font-family: "Sofia", sans-serif;
+        color:red;
+        background: #78a7ff;
+        background: -webkit-linear-gradient(to right, #f5fce3, #e1e6d6 );
+        background: linear-gradient(to right, #f5fce3,#e1e6d6 );
+        background-image: url("images/bg.jpg");
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+      }
+      .center{
+        padding-top:6px;
+        padding-left: 6px;
+        padding-right: 6px;
+        padding-bottom: 6px;
+        display: block;
+        margin-top: 20px;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%
+        
+      }
+      footer{
+        text-align: center;
+      }
+
+      img {
+        border-radius: 10%;
+      }
+      a{
+        color: green;
+      }
+      p{
+        font-family:"Trirong", sans-serif;
+      }
+    </style>
+</head>
+
+<body>
+
+
+<?php include('navbar.php'); ?>
+  
+<div class="center">
+    <h1 style="text-align: center">Welcome to Triples Bank of India<h1>
+    <img class ="center" src="images/Banklogo-2.JPG" alt="" width="150" height="300" >
+</div>
+
+<footer>
+  <p>Copyright © <b>Abhishek Yadav</b> <br><a href=> The Triples Bank of India</a></p>
+</footer>
+
+</body>
+</html>
---
+++ Basic Banking/navbar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/navbar.css">
+</head>
+<body>
+   <nav class="Nav">
+    <ul>
+    <li>
+    <a href="index.php">Home</a>
+    </li>
+    <li>
+    <a href="ViewCustomers.php">Customers Details</a>
+    </li>
+    <li>
+    <a href="Transfer.php">Money Transfer</a>
+    </li>
+    <li>
+    <a href="RecordsPage.php">Transaction History</a>
+    </li>
+    </ul>
+   </nav>
+<body>
+</html>
+
